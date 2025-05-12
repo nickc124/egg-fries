6260ae5cf8faeb830e31104aaada778e5e95506c
nickc124 <nickc14@nycstudents.net>
2025-05-12 10:02:20 -0400

    Update part:a.java

--- Q1/part:a.java
+++ Q1/part:a.java
@@ -2,7 +2,9 @@ public int WalkDog(int Hour){
   int num = company.numAvailableDogs();
   if(MaxDog>=num){
       company.updateDogs(hour,num);
+    return num;
   }else{
       company.updateDogs(hour,MaxDogs);
+    return MaxDogs;
   }
 }
