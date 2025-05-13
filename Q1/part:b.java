7b0480819905ff396b7a1d3315fe31d2dffe1a30
nickc124 <nickc14@nycstudents.net>
2025-05-13 09:36:14 -0400

    Update part:b.java

--- Q1/part:b.java
+++ Q1/part:b.java
@@ -1,5 +1,11 @@
 public int dogWalkShift(int startHour, int endHour){
+  int total=0;
   for(int hour = startHour;hour>=endHour;hour++){
     int numDogs = walkDogs(hour);
+    total+=numdogs*5;
+    if(numDogs==maxDogs || (9<=hour && hour>=17)){
+      total+=3;
+    }
   }
+  return total;
 }
