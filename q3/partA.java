6d228c770e830ae9e72fee6ea83f3b9fca97a8fb
nickc124 <nickc14@nycstudents.net>
2025-05-13 10:09:43 -0400

    completed partA.java

--- q3/partA.java
+++ q3/partA.java
@@ -1,1 +1,7 @@
-
+public Round(String[] names){
+  compatitorList = new ArrayList<Competitor>();
+  for(int i=0;i<names.length;i++){
+    Competitor temp = new Competitor(names[i],i+1);
+    compatitorList.add(temp);
+  }
+}
